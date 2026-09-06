e3dfdf1ab8788538b9ae5588429cb48305f0e86d
Mr Bubs <mr.bubs.mail@gmail.com>
2026-09-06 20:28:10 +0530

    Create books.js

---
+++ assets/js/books.js
@@ -0,0 +1,61 @@
+const KOSHI_BOOKS = {
+  "polynomials": {
+    title: "Polynomials",
+    slug: "polynomials",
+    folder: "Polynomials/pages",
+    complete: false,
+    type: "Original",
+    productionUrl: "../studios/polynomials/",
+    chapters: [
+      { label: "Chapter 1", startPage: 2 },
+      { label: "Chapter 2", startPage: 13 },
+    ],
+  },
+
+  "the-assigned": {
+    title: "The Assigned",
+    slug: "the-assigned",
+    folder: "Assigned/pages",
+    complete: false,
+    type: "Original",
+    productionUrl: "../studios/the-assigned/",
+    chapters: [
+      { label: "Chapter 1", startPage: 1 },
+    ],
+  },
+
+  "art-of-love-and-destruction": {
+    title: "The Art of Love and Destruction",
+    slug: "art-of-love-and-destruction",
+    folder: "art-of-love/pages",
+    complete: true,
+    type: "Creative Adaptation",
+    productionUrl: "../studios/art-of-love-and-destruction/",
+    credit: {
+      text: 'Adapted from "The Art of Destruction" by dilldiva',
+      url: "https://archiveofourown.org/works/68964101",
+    },
+    chapters: [
+      { label: "Chapter 1", startPage: 1 },
+    ],
+  },
+
+  "fallen": {
+    title: "Fallen",
+    slug: "fallen",
+    folder: "fallen/pages",
+    complete: true,
+    type: "Creative Adaptation",
+    productionUrl: "../studios/fallen/",
+    credit: {
+      text: 'Adapted from "Angel of Death" by levitating_marshmallow7',
+      url: "https://archiveofourown.org/works/85885511",
+    },
+    chapters: [
+      { label: "Chapter 1", startPage: 2 },
+      { label: "Chapter 2", startPage: 10 },
+      { label: "Chapter 3", startPage: 19 },
+      { label: "Chapter 4", startPage: 28 },
+    ],
+  },
+};
